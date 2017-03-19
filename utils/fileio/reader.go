05c141ab269389f5650d8cbc0e491fc9091e9f7e
ynqa <un.pensiero.vano@gmail.com>
2017-03-19 17:41:48 +0900

    Add file i/o

---
+++ utils/fileio/reader.go
@@ -0,0 +1,88 @@
+// Copyright © 2017 Makoto Ito
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fileio
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	defaultBatch   = 10000
+	defaultBufSize = 4096
+)
+
+var (
+	rr *bufio.Reader
+)
+
+func Load(inputFile string, f func(words []string)) error {
+	return loadBatch(inputFile, defaultBatch, toLowerWords(f))
+}
+
+func toLowerWords(f func(words []string)) func(sentences []string) {
+	return func(lines []string) {
+		for _, l := range lines {
+			words := strings.Fields(strings.ToLower(l))
+			f(words)
+		}
+	}
+}
+
+func loadBatch(inputFile string, batch int, f func(sentences []string)) error {
+	file, err := os.Open(inputFile)
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	rr = bufio.NewReader(file)
+
+	j := 0
+	for {
+		lines := make([]string, batch)
+		for i := 1; i <= batch; i++ {
+			line, err := readLine()
+			if err == io.EOF {
+				f(lines)
+				return nil
+			} else if err != nil {
+				return err
+			}
+			lines = append(lines, line)
+		}
+		f(lines)
+		j++
+	}
+}
+
+func readLine() (string, error) {
+	buf := make([]byte, 0, defaultBufSize)
+	for {
+		line, isPrefix, err := rr.ReadLine()
+		if err != nil {
+			return "", err
+		}
+
+		buf = append(buf, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buf), nil
+}
---
+++ utils/fileio/writer.go
@@ -0,0 +1,57 @@
+// Copyright © 2017 Makoto Ito
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fileio
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Save(outputPath string, s interface{}) error {
+	dir := extractDir(outputPath)
+
+	if err := os.MkdirAll("."+string(filepath.Separator)+dir, 0777); err != nil {
+		return err
+	}
+
+	file, err := os.Create(outputPath)
+
+	if err != nil {
+		return err
+	}
+	w := bufio.NewWriter(file)
+
+	defer func() {
+		w.Flush()
+		file.Close()
+	}()
+
+	w.WriteString(fmt.Sprintf("%v", s))
+
+	return nil
+}
+
+func FileIsExisted(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func extractDir(path string) string {
+	e := strings.Split(path, "/")
+	return strings.Join(e[:len(e)-1], "/")
+}
