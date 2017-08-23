6842446f4829d1653786d43158df4e919210e26c
ynqa <un.pensiero.vano@gmail.com>
2017-08-23 21:46:05 +0900

    Comment out temporarily

--- cmd/similarity.go
+++ cmd/similarity.go
@@ -14,51 +14,46 @@
 
 package cmd
 
-import (
-	"errors"
-	"fmt"
-
-	"github.com/spf13/cobra"
-	sim "github.com/ynqa/word-embedding/similarity"
-	"github.com/ynqa/word-embedding/utils"
-)
-
-var (
-	rank            int
-	inputVectorFile string
-)
-
-// SimilarityCmd is the command for calculation of similarity.
-var SimilarityCmd = &cobra.Command{
-	Use:     "sim -i FILENAME WORD",
-	Short:   "Estimate the similarity between words",
-	Long:    "Estimate the similarity between words",
-	Example: "word-embedding sim -i example/word_vectors.txt apple",
-	Run: func(cmd *cobra.Command, args []string) {
-		if !inputVectorFileIsExist() {
-			utils.Fatal(fmt.Errorf("InputFile %s is not existed", inputVectorFile))
-		}
-
-		if len(args) == 1 {
-			describe(args[0])
-		} else {
-			utils.Fatal(errors.New("Input a single word"))
-		}
-	},
-}
-
-func init() {
-	SimilarityCmd.Flags().IntVarP(&rank, "rank", "r", 10, "Set number of the similar word list displayed")
-	SimilarityCmd.Flags().StringVarP(&inputVectorFile, "input", "i", "example/word_vectors.txt",
-		"Input path of a file written words' vector with libsvm format")
-}
-
-func describe(w string) {
-	if err := sim.Load(inputVectorFile); err != nil {
-		utils.Fatal(err)
-	}
-
-	if err := sim.Describe(w, rank); err != nil {
-		utils.Fatal(err)
-	}
-}
+//import (
+//	"errors"
+//
+//	"github.com/spf13/cobra"
+//	sim "github.com/ynqa/word-embedding/similarity"
+//	"github.com/ynqa/word-embedding/utils"
+//)
+//
+//var (
+//	rank            int
+//	inputVectorFile string
+//)
+//
+//// SimilarityCmd is the command for calculation of similarity.
+//var SimilarityCmd = &cobra.Command{
+//	Use:     "sim -i FILENAME WORD",
+//	Short:   "Estimate the similarity between words",
+//	Long:    "Estimate the similarity between words",
+//	Example: "word-embedding sim -i example/word_vectors.txt apple",
+//	Run: func(cmd *cobra.Command, args []string) {
+//		if len(args) == 1 {
+//			describe(args[0])
+//		} else {
+//			utils.Fatal(errors.New("Input a single word"))
+//		}
+//	},
+//}
+//
+//func init() {
+//	SimilarityCmd.Flags().IntVarP(&rank, "rank", "r", 10, "Set number of the similar word list displayed")
+//	SimilarityCmd.Flags().StringVarP(&inputVectorFile, "input", "i", "example/word_vectors.txt",
+//		"Input path of a file written words' vector with libsvm format")
+//}
+//
+//func describe(w string) {
+//	if err := sim.Load(inputVectorFile); err != nil {
+//		utils.Fatal(err)
+//	}
+//
+//	if err := sim.Describe(w, rank); err != nil {
+//		utils.Fatal(err)
+//	}
+//}
