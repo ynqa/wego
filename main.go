65989f45da4b107ecddbd52341360ba52a107411
chewxy <chewxy@gmail.com>
2017-09-11 20:42:09 +1000

    Fixed all the comments by @ynqa

--- main.go
+++ main.go
@@ -15,30 +15,13 @@
 package main
 
 import (
-	"log"
 	"os"
-	"runtime/pprof"
 
 	"github.com/ynqa/word-embedding/cmd"
 )
 
 func main() {
-
-	f, err := os.Create("test.prof")
-	if err != nil {
-		log.Fatal(err)
-	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
-
 	if err := cmd.RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-
-	// f, err := os.Create("mem.prof")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// pprof.WriteHeapProfile(f)
-	// f.Close()
 }
