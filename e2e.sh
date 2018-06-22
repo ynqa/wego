6e4f58463606a345c67a0019198dbda8257fda01
ynqa <un.pensiero.vano@gmail.com>
2018-06-22 14:25:02 +0900

    Add e2e

---
+++ e2e.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -e
+
+make build
+
+echo "skip-gram with ns"
+./wego word2vec -i text8 -o example/word2vec_sg_ns.txt --model skip-gram --optimizer ns -d 100 -w 5 --verbose --iter 3 --min-count 5 --thread 20
+echo "skip-gram with hs"
+./wego word2vec -i text8 -o example/word2vec_sg_hs.txt --model skip-gram --optimizer hs -d 100 -w 5 --verbose --iter 3 --min-count 5 --thread 20
+echo "cbow with ns"
+./wego word2vec -i text8 -o example/word2vec_cbow_ns.txt --model cbow --optimizer ns -d 100 -w 5 --verbose --iter 3 --min-count 5 --thread 20
+echo "cbow with hs"
+./wego word2vec -i text8 -o example/word2vec_cbow_hs.txt --model cbow --optimizer hs -d 100 -w 5 --verbose --iter 3 --min-count 5 --thread 20
+
+echo "skip-gram with ns"
+./wego distance -i example/word2vec_sg_ns.txt microsoft
+echo "skip-gram with hs"
+./wego distance -i example/word2vec_sg_hs.txt microsoft
+echo "cbow with ns"
+./wego distance -i example/word2vec_cbow_ns.txt microsoft
+echo "cbow with hs"
+./wego distance -i example/word2vec_cbow_hs.txt microsoft
+
+rm example/word2vec_sg_ns.txt\
+  example/word2vec_sg_hs.txt\
+  example/word2vec_cbow_ns.txt\
+  example/word2vec_cbow_hs.txt
+
+echo "glove with sgd"
+./wego glove -d 50 -i text8 -o example/glove_sgd.txt --iter 10 --thread 12 --initlr 0.05 --min-count 5 -w 15 --solver sgd --verbose
+echo "glove with adagrad"
+./wego glove -d 50 -i text8 -o example/glove_adagrad.txt --iter 10 --thread 12 --initlr 0.05 --min-count 5 -w 15 --solver adagrad --verbose
+
+echo "glove with sgd"
+./wego distance -i example/glove_sgd.txt microsoft
+echo "glove with adagrad"
+./wego distance -i example/glove_adagrad.txt microsoft
+
+rm example/glove_sgd.txt\
+  example/glove_adagrad.txt
+
+rm wego
